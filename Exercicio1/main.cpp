3898454e8e8f5fa3ccfef487d3e00de3200801b0
Matheus Moreira <matheusm920@gmail.com>
2020-08-19 13:38:21 -0300

    Exercício 1 realizado

---
+++ Exercicio1/main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int verificarOrdem(int vetor[], int N)
+{
+    int resultado;
+    int crescente = 0;
+    int decrescente = 0;
+    int igual = 0;
+
+    for(int i = 0; i < N-1; i++)
+    {
+        if(vetor[i] < vetor[i+1])
+        {
+            crescente++;
+        }
+        else if(vetor[i] > vetor[i+1])
+        {
+            decrescente++;
+        }
+        else if(vetor[i] == vetor[i+1])
+        {
+            igual++;
+        }
+    }
+
+    if(igual == (N-1))
+    {
+        resultado = 0;
+    }
+    else if((crescente+igual) == (N-1))
+    {
+        resultado = 1;
+    }
+    else if((decrescente+igual) == (N-1))
+    {
+        resultado = -1;
+    }
+    else
+    {
+        resultado = 99;
+    }
+
+    return resultado;
+
+}
+
+int main()
+{
+    int vetor[] = {1, 2, 3, 4, 10, 20, 20, 20, 20, 33, 44, 55, 66};
+    printf("Resultado: %i", verificarOrdem(vetor, 13));
+}
